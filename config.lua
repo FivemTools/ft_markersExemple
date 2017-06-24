92e0a8778cb32a9fe5a37120972df46d8373dcf5
samuelds <samuelds.pro@gmail.com>
2017-06-24 20:49:17 +0200

    Change location

--- config.lua
+++ config.lua
@@ -1,10 +1,10 @@
 -- @Date:   2017-06-11T23:20:46+02:00
 -- @Project: FiveM Tools
--- @Last modified time: 2017-06-15T12:00:40+02:00
+-- @Last modified time: 2017-06-24T20:35:35+02:00
 -- @License: GNU General Public License v3.0
 
   markers = {
 
-    ft_exemple = { x = 1010.3192138672, y = -2491.8374023438, z = 28.299831390381, type = 1, weight = 5, height = 0.5, red = 158, green = 158, blue = 158, enable = true }
+    ft_exemple = { x = -267.95840454102, y = -958.81805419922, z = 31.223133087158, type = 1, weight = 2, height = 0.5, red = 158, green = 158, blue = 158, enable = true }
 
   }
